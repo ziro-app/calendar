0c75eb429ee6cd530f48df7a6c70e1e823585bca
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-01-24 20:54:58 -0800

    add .env

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const settings = require('./settings.js')
 const calendarAPI = require('node-google-calendar')
 const calendar = new calendarAPI(settings)
 
 const runCalendar = async ({ Events }) => {
 	try {
-		const myEvents = await Events.list(id, {
+		const myEvents = await Events.list(process.env.CALENDAR_ID, {
 			timeMin: '2018-05-20T06:00:00+08:00',
 			timeMax: '2019-05-25T22:00:00+08:00'
 		})
