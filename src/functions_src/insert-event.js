13acb0d98f34682cb1ddff1b287cea752575e74d
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-01-26 20:08:17 -0800

    setup to deploy project as a netlify lambda function

---
+++ src/functions_src/insert-event.js
@@ -0,0 +1,20 @@
+const insertEvent = require('../insertEvent/index')
+
+exports.handler = async ({ httpMethod, queryStringParameters, body }) => {
+	const headers = {
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Headers': 'Content-Type',
+		'Vary': 'Origin'
+	}
+	// const methodOk = httpMethod === 'POST'
+	// const parametersOK = Object.keys(queryStringParameters).length === 0
+	// if (methodOk)
+	const result = await insertEvent()
+	return {
+		headers,
+		statusCode: 200,
+		body: JSON.stringify(result, null, 4)
+	}
+}
+
+// curl -d '{"data": "example"}' -X POST http://localhost:9000/insert-event
--- src/insertEvent/index.js
+++ src/insertEvent/index.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
-const settings = require('./settings.js')
+const settings = require('../settings/index')
 const calendarAPI = require('node-google-calendar')
-const calendar = new calendarAPI(settings)
 
-const runCalendar = async ({ Events }) => {
+const insertEvent = async () => {
 	try {
-		const myEvents = await Events.list(process.env.CALENDAR_ID, {
+		const calendar = new calendarAPI(settings)
+		const myEvents = await calendar.Events.list(process.env.CALENDAR_ID, {
 			timeMin: '2018-05-20T06:00:00+08:00',
 			timeMax: '2019-05-25T22:00:00+08:00'
 		})
@@ -23,10 +23,11 @@ const runCalendar = async ({ Events }) => {
 			],
 			'colorId': 1
 		}
-		const createdEvent = await Events.insert(process.env.CALENDAR_ID, newEvent)
+		const createdEvent = await calendar.Events.insert(process.env.CALENDAR_ID, newEvent)
+		return createdEvent
 	} catch (error) {
 		console.log(JSON.parse(error.message).error.errorBody)
 	}
 }
 
-runCalendar(calendar)
+module.exports = insertEvent
