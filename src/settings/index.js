a7d2ecc665f2c856f3cdc951949f960964890be5
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-01-27 16:44:11 -0800

    fix error using env variables

--- src/settings/index.js
+++ src/settings/index.js
@@ -1,3 +1,7 @@
+console.log(process.env.CLIENT_EMAIL)
+console.log(process.env.PRIVATE_KEY)
+console.log(process.env.CALENDAR_ID)
+
 module.exports.serviceAcctId = process.env.CLIENT_EMAIL
 module.exports.key = process.env.PRIVATE_KEY
 module.exports.timezone = 'UTC+00:00'
