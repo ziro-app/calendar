46df63942eaac12d568d318b23144ee1d08be6aa
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-01-27 18:14:35 -0800

    fix deploy

--- deploy/step.js
+++ deploy/step.js
@@ -1,4 +1,4 @@
 const fs = require('fs')
-fs.writeFileSync('./.env', `PRIVATE_KEY=${process.env.PRIVATE_KEY}\n`)
+fs.writeFileSync('./.env', `PRIVATE_KEY=${process.env.KEY}\n`)
 
 // don't run "npm run build" locally
