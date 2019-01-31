2743c3b67a6cf3c00eef6891b51100c15795eddf
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-01-30 19:59:37 -0800

    add better error handling

--- src/functions_src/insert-event.js
+++ src/functions_src/insert-event.js
@@ -6,14 +6,24 @@ exports.handler = async ({ httpMethod, queryStringParameters, body }) => {
 		'Access-Control-Allow-Headers': 'Content-Type',
 		'Vary': 'Origin'
 	}
-	const methodOk = httpMethod === 'POST'
-	const parametersOK = Object.keys(queryStringParameters).length === 0
-	if (methodOk && parametersOK) {
+	const statusCode = 200
+	const message = {
+		methodError: 'Error. Invalid http method',
+		parametersError: 'Error. Invalid parameters',
+		executionError: 'Error. Internal execution error',
+		ok: 'Success'
+	}
+	let state = 'ok'
+	if (httpMethod !== 'POST')
+		state = 'methodError'
+	if (Object.keys(queryStringParameters).length !== 0)
+		state = 'parametersError'
+	if (state === 'ok') {
 		const result = await insertEvent(JSON.parse(body))
 		return {
 			headers,
-			statusCode: 200,
-			body: JSON.stringify(result, null, 4)
+			statusCode,
+			body: JSON.stringify(message[state], null, 4)
 		}
 	} else {
 		return {
@@ -24,5 +34,5 @@ exports.handler = async ({ httpMethod, queryStringParameters, body }) => {
 	}
 }
 
-// curl -d '{"reseller": "THEWISH COMERCIAL LTDA", "representative": "Rubia", "category": "Troca", "end": "2019-01-30", "time": "17:00:00-02:00", "address": "Av. Tiradentes, 826", "transport": "Aplicativo de Entrega", "packaging": "Sacolas", "invoice": "Karmani, Absolutti"}' -X POST https://calendar.ziro.online/.netlify/functions/insert-event
-// curl -d '{"reseller": "THEWISH COMERCIAL LTDA", "representative": "Rubia", "category": "Troca", "end": "2019-01-30", "time": "17:00:00-02:00", "address": "Av. Tiradentes, 826", "transport": "Aplicativo de Entrega", "packaging": "Sacolas", "invoice": "Karmani, Absolutti"}' -X POST http://localhost:9000/insert-event
+// curl -d '{"reseller": "THEWISH COMERCIAL LTDA", "representative": "Rubia", "category": "Troca", "end": "2019-01-31", "time": "17:00:00-02:00", "address": "Av. Tiradentes, 826", "transport": "Aplicativo de Entrega", "packaging": "Sacolas", "invoice": "Karmani, Absolutti"}' -X POST https://calendar.ziro.online/.netlify/functions/insert-event
+// curl -d '{"reseller": "THEWISH COMERCIAL LTDA", "representative": "Rubia", "category": "Troca", "end": "2019-01-31", "time": "17:00:00-02:00", "address": "Av. Tiradentes, 826", "transport": "Aplicativo de Entrega", "packaging": "Sacolas", "invoice": "Karmani, Absolutti"}' -X POST http://localhost:9000/insert-event
