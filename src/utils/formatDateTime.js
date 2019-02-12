5a176a09471a35ee4ac4da7ab71a1aba78e23e21
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-02-11 21:25:38 -0800

    fix date-time formatting

--- src/utils/formatDateTime.js
+++ src/utils/formatDateTime.js
@@ -1,8 +1,25 @@
 const moment = require('moment-timezone')
 
-const formatDateTime = (date, time) =>
-	moment(`${date}T${time}:00`).tz('America/Sao_Paulo').format()
+const monthToNumber = month => {
+	switch (month) {
+		case 'jan': return '01'
+		case 'fev': return '02'
+		case 'mar': return '03'
+		case 'abr': return '04'
+		case 'mai': return '05'
+		case 'jun': return '06'
+		case 'jul': return '07'
+		case 'ago': return '08'
+		case 'set': return '09'
+		case 'out': return '10'
+		case 'nov': return '11'
+		case 'dez': return '12'
+	}
+}
+
+const formatDateTime = (date, time) => {
+	const dateTime = `${date.substring(7,11)}-${monthToNumber(date.substring(3,6))}-${date.substring(0,2)} ${time}`
+	return moment.tz(dateTime, 'America/Sao_Paulo').format()
+}
 
 module.exports = formatDateTime
-
-// '2019-02-14T00:00:00-00:00'
